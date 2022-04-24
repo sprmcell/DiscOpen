9ce9d64acb42fc4d378458045f459ab612e8c74d
YomoTho <vrekmakerq@gmail.com>
2022-04-24 19:03:25 +0200

    Removed 2 function in common.cpp

--- core/common/src/common.cpp
+++ core/common/src/common.cpp
@@ -1,18 +1,6 @@
 #include "common.h"
 
 
-const char* homedir()
-{
-    return getenv("HOME");
-}
-
-const char* cache_dir()
-{
-    if (!getenv("XDG_CACHE_HOME"))
-        putenv("XDG_CACHE_HOME=$HOME/.cache");
-    return getenv("XDG_CACHE_HOME");
-}
-
 u_int8_t random_8_id()
 {
     uint8_t num = rand() % 255 + 1;
